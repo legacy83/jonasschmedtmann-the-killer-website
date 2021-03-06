e1e444ce0a8ba61f16b5ce42f4b229b7acfbd542
Thiago Senna <trsenna@gmail.com>
2021-03-06 15:52:41 -0300

    reformating only

--- omnifood/functions.php
+++ omnifood/functions.php
@@ -1,7 +1,7 @@
 <?php
 
-define( 'APP_THEME', true );
-define( 'APP_THEME_FILE', __FILE__ );
+define('APP_THEME', true);
+define('APP_THEME_FILE', __FILE__);
 
 # ------------------------------------------------------------------------------
 # Lorem ipsum dolor sit amet
@@ -11,15 +11,14 @@ define( 'APP_THEME_FILE', __FILE__ );
 # cursus. Vestibulum dictum nisi ligula, in dictum justo pulvinar quis.
 #
 
-add_action( 'mezu/bootstrap', function ( \Dalen\theme $theme ) {
+add_action('mezu/bootstrap', function (\Dalen\theme $theme) {
 
-    $theme->register( new \App\Theme\Assets\Scripts() );
-    $theme->register( new \App\Theme\Assets\Styles() );
-    $theme->register( new \App\Theme\Sanse\Background() );
-    $theme->register( new \App\Theme\Sanse\Comments() );
-    $theme->register( new \App\Theme\Sanse\Header() );
-    $theme->register( new \App\Theme\Sanse\Menus() );
-    $theme->register( new \App\Theme\Sanse\Sidebars() );
-    $theme->register( new \App\Theme\Sanse\Templates() );
-
-} );
+    $theme->register(new \App\Theme\Assets\Scripts());
+    $theme->register(new \App\Theme\Assets\Styles());
+    $theme->register(new \App\Theme\Sanse\Background());
+    $theme->register(new \App\Theme\Sanse\Comments());
+    $theme->register(new \App\Theme\Sanse\Header());
+    $theme->register(new \App\Theme\Sanse\Menus());
+    $theme->register(new \App\Theme\Sanse\Sidebars());
+    $theme->register(new \App\Theme\Sanse\Templates());
+});
