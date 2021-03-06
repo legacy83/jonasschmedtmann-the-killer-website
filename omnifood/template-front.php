62f64faf20bd3d195176d3231eb397df24586443
Thiago Senna <trsenna@gmail.com>
2021-03-06 16:22:14 -0300

    salvo resources necessarios para criacao da pagina

--- omnifood/template-front.php
+++ omnifood/template-front.php
@@ -20,6 +20,29 @@
 
 <body <?php body_class(); ?>>
 
+	<header>
+		<nav>
+			<div class="row">
+				<img src="<?= get_theme_file_uri('resources/content/img/logo-white.png'); ?>" alt="Omnifood Logo" class="logo">
+				<img src="<?= get_theme_file_uri('resources/content/img/logo.png'); ?>" alt="Omnifood Logo" class="logo-black">
+				<ul class="main-nav js--main-nav">
+					<li><a href="#features">Food delivery</a></li>
+					<li><a href="#works">How it works</a></li>
+					<li><a href="#cities">Our cities</a></li>
+					<li><a href="#plans">Sign up</a></li>
+				</ul>
+				<a class="mobile-nav-icon js--nav-icon">
+					<i class="ion-navicon-round"></i>
+				</a>
+			</div>
+		</nav>
+		<div class="hero-text-box">
+			<h1>Goodbye junk food.<br>Hello super healthy meals.</h1>
+			<a class="btn btn-full js--scroll-to-plans" href="#">I'm hungry</a>
+			<a class="btn btn-ghost js--scroll-to-start" href="#">Show me more</a>
+		</div>
+	</header>
+
 	<?php wp_footer(); ?>
 
 </body>
