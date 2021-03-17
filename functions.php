9086434bc36d635025fb7ad177bcedd5272f74ea
Thiago Senna <trsenna@gmail.com>
2021-03-16 22:48:29 -0300

    salvando progresso

--- functions.php
+++ functions.php
@@ -17,3 +17,33 @@ use Dalen\Theme;
 add_action('mezu/bootstrap', function (Theme $theme) {
     $theme->register(new Styles);
 });
+
+# ------------------------------------------------------------------------------
+# Lorem ipsum dolor sit amet
+# ------------------------------------------------------------------------------
+#
+# Suspendisse sodales ipsum non justo imperdiet, ut lacinia erat
+# cursus. Vestibulum dictum nisi ligula, in dictum justo pulvinar quis.
+#
+
+add_action('after_setup_theme', function () {
+    add_theme_support('editor-styles');
+    add_editor_style('template-home.css');
+
+    add_theme_support('align-wide');
+
+    add_theme_support('disable-custom-colors');
+
+    add_theme_support('editor-color-palette', [
+        [
+            'name'  => 'Orange #1',
+            'slug'  => 'orange-1',
+            'color' => '#e67e22',
+        ],
+        [
+            'name'  => 'Orange #2',
+            'slug'  => 'orange-2',
+            'color' => '#cf6d17',
+        ]
+    ]);
+});
