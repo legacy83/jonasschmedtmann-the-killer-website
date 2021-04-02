285214f76a85aae57b6d1c17a1288178d84616c7
Thiago Senna <trsenna@gmail.com>
2021-04-02 20:01:27 -0300

    styles moved to functions assets

--- includes/functions-assets.php
+++ includes/functions-assets.php
@@ -11,3 +11,14 @@ add_action('wp_enqueue_scripts', function () {
     wp_dequeue_style('sanse-parent-style');
     wp_dequeue_style('sanse-style');
 });
+
+/*  -----------------------------------------------------------------------------------------------
+    LOREM IPSUM
+    Suspendisse sodales ipsum non justo imperdiet cursus.
+--------------------------------------------------------------------------------------------------- */
+
+add_action('wp_enqueue_scripts', function () {
+    if (is_page_template('template-front.php')) {
+        wp_enqueue_style('app/theme/template-front', get_theme_file_uri('template-front.css'));
+    }
+});
--- template-front.php
+++ template-front.php
@@ -10,13 +10,6 @@ use App\Theme\Reusable\BlockAreaFactory;
 
 use function Mezu\view;
 
-add_action('wp_enqueue_scripts', function () {
-	wp_enqueue_style(
-		'app/theme/template-front',
-		get_theme_file_uri('template-front.css')
-	);
-});
-
 add_action('app/theme/header', BlockAreaFactory::make('header'));
 add_action('app/theme/footer', BlockAreaFactory::make('footer'));
 add_action('app/theme/main', ContentFactory::make());
